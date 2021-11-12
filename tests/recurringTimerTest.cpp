e33039e8f2ec55d8174e7fc61e504d108db359c8
steveio.com <info@steveio.com>
2021-11-12 20:18:01 +0000

    Callback on timer event

--- tests/recurringTimerTest.cpp
+++ tests/recurringTimerTest.cpp
@@ -11,6 +11,24 @@ using namespace std;
 timeb t_start;
 RecurringTimer timer;
 
+class DeviceClass
+{
+public:
+   bool Activate() { printf("DeviceClass::Activate\n"); return true; };
+   bool DeActivate() { printf("DeviceClass::DeActivate\n"); return true; };
+};
+
+DeviceClass device;
+
+void activateDevice()
+{
+  device.Activate();
+}
+
+void (*pt2Function)() = NULL;
+//bool (DeviceClass::*ptCallback)() = NULL;
+
+
 unsigned long millis() {
   timeb t_now;
   ftime(&t_now);
@@ -27,23 +45,40 @@ void setup()
 {
   printf("RecurringTimer test\n");
 
-  struct tmElements_t t1_on;
-
-  t1_on.Hour = 9;
-  t1_on.Min = 0;
-  t1_on.Sec = 0;
-
   unsigned long ts = millis() / 1000;
   unsigned long interval = 1000 * 5;
 
-  //timer.init(TIMER_MILLIS_RECURRING, ts, &t1_on, interval);
+  /**
+  // start timer from specific point in time
 
+  struct tmElements_t t1_on;
 
+  t1_on.Hour = 9;
+  t1_on.Min = 30;
+  t1_on.Sec = 0;
+
+  timer.init(TIMER_MILLIS_RECURRING, ts, &t1_on, interval);
+  **/
+
+  // start timer from now
   timer.init(TIMER_MILLIS_RECURRING, ts, interval);
 
-  // init as object pointer
-  //RecurringTimer* timer = new RecurringTimer();
-  //timer->init(TIMER_MILLIS_RECURRING, ts, &t1_on, interval);
+  // set callback func
+  pt2Function = activateDevice;
+  if (pt2Function > 0)
+  {
+    timer.setCallback(pt2Function);
+  }
+
+  /*
+  ptCallback = &DeviceClass::Activate;
+  if(ptCallback == &DeviceClass::Activate)
+  */
+
+  /* init timer as pointer
+  RecurringTimer* timer = new RecurringTimer();
+  timer->init(TIMER_MILLIS_RECURRING, ts, &t1_on, interval);
+  */
 
 }
 
