5e7ecd12ac5bfe0cb7a53f7f12ec7c7aa26bd1e6
steveio.com <info@steveio.com>
2021-12-09 20:06:23 +0000

    Func based timer

--- FunctionTimer.cpp
+++ FunctionTimer.cpp
@@ -21,13 +21,6 @@ void FunctionTimer::init(int t, unsigned long ts, unsigned long interval)
   _startTs = ts;
   _interval = interval;
 
-  if (_type == TIMER_FUNCTION)
-  {
-    _timerCallback = &getNextEventIncremental;
-  } else if (_type == TIMER_FUNCTION_EXP)
-  {
-    _timerCallback = &getNextEventExPow;
-  }
   // compute timer next event + timeout
   _setNextEvent(_startTs);
 }
@@ -36,14 +29,24 @@ void FunctionTimer::init(int t, unsigned long ts, unsigned long interval)
 void FunctionTimer::_setNextEvent(unsigned long ts)
 {
   _prevEvent = ts;
-  if(_timerCallback)
+  if (_type == TIMER_FUNCTION)
   {
-    if (_type == TIMER_FUNCTION)
+    if(_timerDefaultFunc)
     {
-      call(_timerCallback(&_nextEvent, _prevEvent, _interval, ts));
-    } else if (_type == TIMER_FUNCTION_EXP)
-    {
-      call(_timerCallback(&_nextEvent, _prevEvent, _interval, ts, expow_pow, expow_dir, expow_max, expow_rev));
+      (*_timerDefaultFunc)(&_nextEvent, _prevEvent, _interval, ts);
     }
+  } else if (_type == TIMER_FUNCTION_EXP)
+  {
+    if(_timerExpFunc)
+    {
+      (*_timerExpFunc)(&_nextEvent, _prevEvent, _interval, ts, timer_expow_pow, timer_expow_dir, timer_expow_max, timer_expow_rev);
+    }
+  } else if (_type == TIMER_FUNCTION_RAND)
+  {
+    if(_timerRandFunc)
+    {
+      (*_timerRandFunc)(&_nextEvent, _prevEvent, ts, timer_rand_lower, timer_rand_upper);
+    }
+
   }
 }
--- FunctionTimer.h
+++ FunctionTimer.h
@@ -18,22 +18,24 @@ Copyright (C) 2020  Steven G Edwards
 
 #define TIMER_FUNCTION 0x10
 #define TIMER_FUNCTION_EXP 0x11
+#define TIMER_FUNCTION_RAND 0x12
 
 
-typedef void (*pt2TimerFunction)(unsigned long * currEvent, unsigned long prevEvent, unsigned long interval, unsigned long ts);
-typedef void (*pt2TimerFunction)(unsigned long * currEvent, unsigned long prevEvent, unsigned long interval, unsigned long ts, int pow, bool dir, unsigned long max, bool rev);
+typedef void (*pt2TimerDefaultFunction)(unsigned long * currEvent, unsigned long prevEvent, unsigned long interval, unsigned long ts);
+typedef void (*pt2TimerExpFunction)(unsigned long * currEvent, unsigned long prevEvent, unsigned long interval, unsigned long ts, int pow, bool dir, unsigned long max, bool rev);
+typedef void (*pt2TimerRandFunction)(unsigned long * currEvent, unsigned long prevEvent, unsigned long ts, unsigned long _upper, unsigned long _lower);
 
 // default timer callback: recurring timer
-void getNextEventIncremental(unsigned long * nextEvent, unsigned long prevEvent, unsigned long interval, unsigned long ts)
+void getNextEvent(unsigned long * nextEvent, unsigned long prevEvent, unsigned long interval, unsigned long ts)
 {
   nextEvent = ts + interval;
 }
 
 // exponential timer incremental/decremental starting at interval upto zero / limit (max) with option to ping-pong
-int expow_pow = 2;
-bool expow_dir = 0;
-unsigned long expow_max = SECS_PER_MIN * 5;
-bool expow_rev = 0;
+int timer_expow_pow = 2;
+bool timer_expow_dir = 0;
+unsigned long timer_expow_max = SECS_PER_MIN * 5;
+bool timer_expow_rev = 0;
 
 void getNextEventExPow(unsigned long * nextEvent, unsigned long prevEvent, unsigned long interval, unsigned long ts, int _pow, bool _dir, unsigned long _max, bool _rev)
 {
@@ -46,7 +48,7 @@ void getNextEventExPow(unsigned long * nextEvent, unsigned long prevEvent, unsig
       {
         nextEvent = ts + (diff * _pow);
       } else if (_rev = 1) {
-        expow_dir = 1;
+        timer_expow_dir = 1;
         nextEvent = ts;
       }
     } else {
@@ -54,7 +56,7 @@ void getNextEventExPow(unsigned long * nextEvent, unsigned long prevEvent, unsig
       {
         nextEvent = ts - (diff / _pow);
       } else {
-        expow_dir = 0;
+        timer_expow_dir = 0;
         nextEvent = ts;
       }
     }
@@ -62,6 +64,23 @@ void getNextEventExPow(unsigned long * nextEvent, unsigned long prevEvent, unsig
     nextEvent = ts + interval;
   }
 }
+
+unsigned long timer_rand_lower = 1000;
+unsigned long timer_rand_upper = 200000;
+
+
+void getNextEventRand(unsigned long * nextEvent, unsigned long prevEvent, unsigned long ts, unsigned long _upper, unsigned long _lower)
+{
+
+  unsigned long interval = (rand() % (_upper - _lower + 1)) + _lower;
+  nextEvent = ts + interval;
+}
+
+
+pt2TimerDefaultFunction timerDefaultFunc = &getNextEvent;
+pt2TimerExpFunction timerExpFunc = &getNextEventExPow;
+pt2TimerRandFunction timerRandFunc = &getNextEventRand;
+
 
 class FunctionTimer: public Timer
 {
@@ -71,18 +90,30 @@ public:
   FunctionTimer();
   void init(int t, unsigned long ts, unsigned long interval);
 
-  void setTimerCallback(pt2TimerFunction ptCallback)
+  void setTimerCallback(pt2TimerDefaultFunction ptCallback)
   {
-    this->_timerCallback = ptCallback;
-  }
+    _timerDefaultFunc = ptCallback;
+  };
 
-  unsigned long getNextEvent();
+  /*
+  void setTimerCallback(pt2TimerExpFunction ptCallback)
+  {
+    _timerExpFunc = ptCallback;
+  };
+
+  void setTimerCallback(pt2TimerRandFunction ptCallback)
+  {
+    _timerRandFunc = ptCallback;
+  };
+  */
 
   protected:
 
   private:
     void _setNextEvent(unsigned long ts);
-    pt2Function _timerCallback = NULL;
+    pt2TimerDefaultFunction _timerDefaultFunc = timerDefaultFunc;
+    pt2TimerExpFunction _timerExpFunc = timerExpFunc;
+    pt2TimerRandFunction _timerRandFunc = timerRandFunc;
 };
 
 
